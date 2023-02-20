6a2e930eb060bdece46cd33980c0fdf9c9586f67
shanid544 <shanidvpputhanpeedika@gmail.com>
2023-02-20 11:26:33 +0530

    Add files via upload

---
+++ sprinbootdemo/src/main/java/com/example/sprinbootdemo/SprinbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.sprinbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootdemoApplication.class, args);
+	}
+
+}
